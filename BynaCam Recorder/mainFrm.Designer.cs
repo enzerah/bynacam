0df9c8c0dff8d668e8de78051b489260d8fb9b97
beziak <beziak@o2.pl>
2009-02-03 11:28:38 +0000

    Recorder - Added loading Tibia path from Ini Config.

--- BynaCam Recorder/mainFrm.Designer.cs
+++ BynaCam Recorder/mainFrm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BynaCam_Recorder
 {
-    partial class Form1
+    partial class mainFrm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainFrm));
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.saveAndExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
