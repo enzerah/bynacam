f594435184596b258fb1492db0f6433a3338dde9
beziak@o2.pl <beziak@o2.pl@f8cffbfe-f16b-11dd-a8e2-f7cb3c35fcff>
2009-02-03 02:13:54 +0000

    Added GameServer.dll Added hotkeys to change playing speed BynaCam Player Rewritten

---
+++ BynaCam/TibiaNetwork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Tibia.Objects;
+
+namespace BynaCam
+{
+    class TibiaNetwork
+    {
+        public GameServer.GameServer uxGameServer;
+        public TibiaNetwork(Client client)
+        {
+            uxGameServer = new GameServer.GameServer(client);
+            LoginServer.Login login = new LoginServer.Login();
+
+            Thread t = new Thread(new ThreadStart(delegate()
+            {
+                login.StartServer(client, 7171, "Byna", "BynaCam", new byte[] { 127, 0, 0, 1 }, 7172);
+                uxGameServer.SetServer(7172);
+            }));
+            t.Start();
+        }
+    }
+}
