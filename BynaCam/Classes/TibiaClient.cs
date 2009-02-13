ccc8d9540882984bb8ae76b4f5ef0abfd83cb466
beziak <beziak@o2.pl>
2009-02-13 01:03:17 +0000

    Forgot add some files

---
+++ BynaCam/Classes/TibiaClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tibia.Objects;
+
+namespace BynaCam
+{
+    public static class TibiaClient
+    {
+        public static void updateTitle(Client c, double speed, TimeSpan actualTime, TimeSpan movieTime)
+        {
+            c.Title = "BynaCam :: Speed x"+speed+" :: Time["+actualTime.Hours+":"+actualTime.Minutes+":"+actualTime.Seconds+" / "+movieTime.Hours+":"+movieTime.Minutes+":"+movieTime.Seconds+"]";
+        }
+    }
+}
